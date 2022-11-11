b9cc5439f8d9248fe01adfda9de47616f5e479d7
Hugo Deliere <hudelier@e1r7p18.clusters.42paris.fr>
2022-11-11 10:55:28 +0100

    piscine

---
+++ testexam/plit.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   plit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hudelier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 07:09:07 by hudelier          #+#    #+#             */
+/*   Updated: 2022/10/07 07:25:04 by hudelier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	nb_w(char *str)
+{
+	int	c;
+
+	c = 0;
+	while (str && *str && (*str == ' ' || *str == '\n' || *str == '\t'))
+		str++;
+	if (str && *str && *str != ' ' && *str != '\n' && *str != '\t')
+		c++;
+	while (str && *str && *str != ' ' && *str != '\n' && *str != '\t')
+		str++;
+	if (str && *str && (*str == ' ' || *str == '\n' || *str == '\t'))
+		c += nb_w(str);
+	return (c);
+}
+
+int	nb_l(char *str)
+{
+	int	l;
+
+	l = 0;
+	while (str[l] && str[l] != ' ' && str[l] != '\n' && str[l] != '\t')
+		l++;
+	return (l);
+}
+
+char	**ft_split(char *str)
+{
+	int	nbw;
+	int	i;
+	int	j;
+	char	**dest;
+
+	nbw = nb_w(str);
+	dest = malloc(sizeof(char *) * (nbw +1));
+	if (!dest || !str)
+		return (0);
+	i = -1;
+	while (++i < nbw)
+	{
+		j = 0;
+		while (str && *str && (*str == ' ' || *str == '\n' || *str == '\t'))
+			str++;
+		dest[i] = malloc(sizeof(char) * (nb_l(str) + 1));
+		while (dest[i][j])
+		{
+			dest[i][j++] = *str;
+			str++;
+		}
+		dest[i][j] = 0;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+int	main(int ac, char **av)
+{
+	char	**dest;
+	int	i;
+
+	i = -1;
+	dest = ft_split(av[1]);
+	while(dest[++i])
+		printf("%s\n", dest[i]);
+	return (0);
+}
